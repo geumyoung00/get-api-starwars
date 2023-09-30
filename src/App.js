c52cfa451981c6c02c65687add5ef1afc5467a8e
geumyoung <1gyn093@gmail.com>
2023-09-30 17:48:14 +0900

    api호출 및 객체배열 데이터 만들기, movies데이터 undefined

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
+// API : https://swapi.dev/api/films
 
 function App() {
+  const [movies, setMovies] = useState([]);
+
+  const getMovies = async (word) => {
+    const res = await fetch('https://swapi.dev/api/films');
+    console.log('res___', res);
+    const json = await res.json();
+    console.log('json___', json);
+    // API에서 필요한 정보만 배열로 담기 (map method)
+    const moviesList = json.results.map((movie) => {
+      // moveis = {id, title, openingText, releaseDate}
+      return {
+        id: movie.episode_id,
+        title: movie.title,
+        openingText: movie.opening_crawl,
+        releaseDate: movie.releaseDate,
+      };
+    });
+    setMovies(moviesList);
+    console.log('movies___', movies);
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
   const dummyMovies = [
     {
       id: 1,
