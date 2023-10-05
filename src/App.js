433c4a1be84aab65545af0c4ec449912a196809e
geumyoung <1gyn093@gmail.com>
2023-10-05 21:52:16 +0900

    api호출하기

--- src/App.js
+++ src/App.js
@@ -13,46 +13,29 @@ function App() {
     const json = await res.json();
     console.log('json___', json);
     // API에서 필요한 정보만 배열로 담기 (map method)
-    const moviesList = json.results.map((movie) => {
+    const movieList = json.results.map((movie) => {
       // moveis = {id, title, openingText, releaseDate}
       return {
         id: movie.episode_id,
         title: movie.title,
         openingText: movie.opening_crawl,
-        releaseDate: movie.releaseDate,
+        releaseDate: movie.release_date,
       };
     });
-    setMovies(moviesList);
-    console.log('movies___', movies);
+    setMovies(movieList);
+    console.log(movies);
   };
 
   useEffect(() => {
     getMovies();
   }, []);
 
-  const dummyMovies = [
-    {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
-    {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
-    },
-  ];
-
   return (
     <React.Fragment>
       <section>
         <button>Fetch Movies</button>
       </section>
-      <section>
-        <MoviesList movies={dummyMovies} />
-      </section>
+      <section>{'' && <MoviesList movies={movies} />}</section>
     </React.Fragment>
   );
 }
